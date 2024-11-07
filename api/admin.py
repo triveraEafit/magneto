2d2c60982a273688103ac37d8caa1726acc6218d
DavidRamirez1106 <davidramirez.a4@gmail.com>
2024-11-07 10:54:14 -0500

    Imagen

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import HojaDeVida
+from .models import ImagenPegada
+
+admin.site.register(HojaDeVida)
+admin.site.register(ImagenPegada)
